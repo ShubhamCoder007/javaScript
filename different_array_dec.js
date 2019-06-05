e4e1dd740a296e26f8a1a54a31076cdb23ee9bb5
Shubham Banerjee <32201980+ShubhamCoder007@users.noreply.github.com>
2019-06-05 21:35:42 +0530

    Add files via upload

---
+++ different_array_dec.js
@@ -0,0 +1,18 @@
+//method 1
+var num = new Array(1,2,3,4,5);
+
+//method 2
+var num1 = new Array(2);
+num1[0] = 1;
+num1[1] = 2;
+
+//method 3
+var num2 = new Array();
+num2[0] = 1;
+num1[1] = 2;
+
+
+document.write("num: "+num+"<br>");
+document.write("num1: "+num1+"<br>");
+document.write("num2: "+num2);
+
